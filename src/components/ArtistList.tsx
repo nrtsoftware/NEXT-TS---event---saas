65261589f545f46a39ff7a0f9b3e9d4a1a269eed
Nivs <nivsoficial@gmail.com>
2023-07-15 19:47:18 -0300

    ATTs

--- src/components/ArtistList.tsx
+++ src/components/ArtistList.tsx
@@ -2,54 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { FaFacebook, FaInstagram, FaSoundcloud } from 'react-icons/fa';
-
-const artists = [
-  { name: 'Artista 1', time: '18h30', genre: 'Pop', playing: true, ended: false, started: true, socialMedias: {
-    facebook: 'sefoda',  time: '18h30',  instagram: 'style', soundcloud: 'fuacaquente'
-  }, photo: 'artist1.png' },
-  { name: 'Artista 2',  time: '18h30',  genre: 'Rock', playing: false, ended: false, started: false,socialMedias: {
-    facebook: 'kopdspo', instagram: 'saddsa', soundcloud: 'asddsa'
-  }, photo: 'artist2.png' },
-  { name: 'Artista 3',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: 'saddsa', instagram: '', soundcloud: ''
-  }, photo: 'artist3.png' },
-  { name: 'Artista 4',  time: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: 'dsadas', soundcloud: ''
-  }, photo: 'artist5.png' },
-  { name: 'Artista 5',  time: '18h30',  genre: 'Reggae', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: 'dasdsa'
-  }, photo: 'artist6.png' },
-  { name: 'Artista 6',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: 'dsadsa', instagram: '', soundcloud: ''
-  }, photo: 'artist7.png' },
-  { name: 'Artista 7',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: 'artist8.png' },
-  { name: 'Artista 8',  time: '18h30', genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: '' },
-  { name: 'Artista 9',  time: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: 'artist9.png' },
-  { name: 'Artista 10',  time: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: 'artist10.png' },
-  { name: 'Artista 11',  time: '18h30', genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: 'artist11.png' },
-  { name: 'Artista 12',  time: '18h30',  genre: 'Psytrance', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: '' },
-  { name: 'Artista 13',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: '' },
-  { name: 'Artista 14',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: '' },
-  { name: 'Artista 15',  time: '18h30', genre: 'Hip Hop', playing: false, ended: true, started: true,socialMedias: {
-    facebook: '', instagram: '', soundcloud: ''
-  }, photo: '' },
-];
+import artists from './artistlist.data';
 
 const ArtistList = () => {
   const [selectedGenre, setSelectedGenre] = useState('');

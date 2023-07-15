dbf812ff463fc57653605895c6af67c3a15fd10c
Nivs <nivsoficial@gmail.com>
2023-07-15 19:51:41 -0300

    feat: new mocked data about artistlist [WIP]

--- src/components/artistlist.data.ts
+++ src/components/artistlist.data.ts
@@ -1,47 +1,92 @@
 export default [
-    { name: 'Artista 1', time: '18h30', genre: 'Pop', playing: true, ended: false, started: true, socialMedias: {
-      facebook: 'sefoda',  time: '18h30',  instagram: 'style', soundcloud: 'fuacaquente'
+    { name: 'Artista 1', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30', genre: 'Pop', playing: true, ended: false, started: true, socialMedias: {
+      facebook: 'sefoda',  timeDisplay: '18h30',  instagram: 'style', soundcloud: 'fuacaquente'
     }, photo: 'artist1.png' },
-    { name: 'Artista 2',  time: '18h30',  genre: 'Rock', playing: false, ended: false, started: false,socialMedias: {
+    { name: 'Artista 2', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Rock', playing: false, ended: false, started: false,socialMedias: {
       facebook: 'kopdspo', instagram: 'saddsa', soundcloud: 'asddsa'
     }, photo: 'artist2.png' },
-    { name: 'Artista 3',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 3', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: 'saddsa', instagram: '', soundcloud: ''
     }, photo: 'artist3.png' },
-    { name: 'Artista 4',  time: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 4',  realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: 'dsadas', soundcloud: ''
     }, photo: 'artist5.png' },
-    { name: 'Artista 5',  time: '18h30',  genre: 'Reggae', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 5', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Reggae', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: 'dasdsa'
     }, photo: 'artist6.png' },
-    { name: 'Artista 6',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 6', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: 'dsadsa', instagram: '', soundcloud: ''
     }, photo: 'artist7.png' },
-    { name: 'Artista 7',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 7', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: 'artist8.png' },
-    { name: 'Artista 8',  time: '18h30', genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 8', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30', genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: '' },
-    { name: 'Artista 9',  time: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 9', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: 'artist9.png' },
-    { name: 'Artista 10',  time: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 10', realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Hip Hop', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: 'artist10.png' },
-    { name: 'Artista 11',  time: '18h30', genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 11',  realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30', genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: 'artist11.png' },
-    { name: 'Artista 12',  time: '18h30',  genre: 'Psytrance', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 12',  realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Psytrance', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: '' },
-    { name: 'Artista 13',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 13',  realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: '' },
-    { name: 'Artista 14',  time: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
+    { name: 'Artista 14',  realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30',  genre: 'Jazz', playing: false, ended: false, started: false, socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: '' },
-    { name: 'Artista 15',  time: '18h30', genre: 'Hip Hop', playing: false, ended: true, started: true,socialMedias: {
+    { name: 'Artista 15',  realTime: {
+      "init_time": "2023-07-15T10:00:00",
+      "end_time": "2023-07-15T15:00:00",
+}, timeDisplay: '18h30', genre: 'Hip Hop', playing: false, ended: true, started: true,socialMedias: {
       facebook: '', instagram: '', soundcloud: ''
     }, photo: '' },
   ];

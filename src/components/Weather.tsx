48a439660f7e6dc8512a12c2c81c3b2e7af720c5
Nivs <nivsoficial@gmail.com>
2023-07-15 17:55:51 -0300

    fix: atts about temperature [ON/OFF switcher]

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -3,15 +3,22 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Weather(props: any) {
-    const clima = new Clima(props.city);
-    const [tempCelsius, setTempCelsius] = useState('0°C');
-    const [icon, setIcon] = useState('');
-    clima.getData().then((e) => {
-      setIcon('https:' + e.current.condition.icon);
-      setTempCelsius(e.current.temp_c + '°C');
-    });
+    const ON = false;
+    if(ON) {
+        const clima = new Clima(props.city);
+        const [tempCelsius, setTempCelsius] = useState('0°C');
+        const [icon, setIcon] = useState('');
+        clima.getData().then((e) => {
+          setIcon('https:' + e.current.condition.icon);
+          setTempCelsius(e.current.temp_c + '°C');
+        });
+        return <>
+        Temperature: {tempCelsius}
+        <Image alt="Sol" src={icon} width="40" height="40"/>
+        </>
+    }
     return <>
-    Temperature: {tempCelsius}
-    <Image alt="Sol" src={icon} width="40" height="40"/>
+        API de Temperatura desabilitada.
     </>
+
 }
